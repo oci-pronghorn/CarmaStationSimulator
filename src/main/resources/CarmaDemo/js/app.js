82b13e22c890d5fbc1a96a9a72256d2f89fe89b9
Nathan Tippy <nathan@tippy.name>
2015-10-25 12:30:09 -0500

    update with websocket logic.

--- src/main/resources/CarmaDemo/js/app.js
+++ src/main/resources/CarmaDemo/js/app.js
@@ -8,8 +8,9 @@ const app = angular.module('myApp', [
 ]);
 console.log('app.js x: created myApp module');
 
+
 app.config(['$routeProvider', $routeProvider => {
-  $routeProvider.when('/admin', {
+ $routeProvider.when('/admin', {
     templateUrl: 'partials/admin.html',
     controller: 'MyCtrl',
     view: 'center'
@@ -29,21 +30,89 @@ app.config(['$routeProvider', $routeProvider => {
     controller: 'MyCtrl',
     view: 'center'
   });
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.otherwise({redirectTo: '/admin'});
 }]);
 
-app.controller('MyCtrl', $scope => {
-  if (!WebSocket) window.WebSocket = window.MozWebSocket;
-  if (!WebSocket) {
-    return alert('Your browser does not support WebSockets.');
-  }
 
-  const socket = new WebSocket('ws://127.0.0.1:8080/websocket');
-  socket.onmessage = event => $scope.message += '\n' + event.data;
-  socket.onclose = () => $scope.message = 'WebSocket closed';
-  socket.onopen = () => $scope.message = 'WebSocket opened';
+app.controller('MyCtrl', (function($scope) {
+	
+	  if (!WebSocket) window.WebSocket = window.MozWebSocket;
+	  if (!WebSocket) {
+		    return alert('Your browser does not support WebSockets.');
+	  }
 
-  function updatePumpStatus() {
+	  var socket = null;
+
+	  function startupWebSocket() {
+	  	   socket = new WebSocket("ws://"+location.host+"/websocket");
+	      
+	       socket.binaryType = 'arraybuffer';
+	       socket.onmessage = function(event) {
+				    	
+		            var view = new Uint8Array(event.data);
+
+   	                var dataView = new Uint32Array(event.data);	 
+   	                
+   	                
+   	                //Do something with the data               	        
+   	                	         
+
+	       }
+	       
+	      socket.onopen = function(event) {
+				   console.log('Web Socket opened!');
+		  }
+		  
+		  socket.onclose = function(event) {
+				    console.log('Web Socket closed');
+				    checkWebSocketStatus();
+				    
+		  }
+
+	  }
+	  
+	  function checkWebSocketStatus(){
+   			 if(!socket || socket.readyState == 3) startupWebSocket();
+  	  }
+  	  
+	  startupWebSocket();
+	  setInterval(checkWebSocketStatus, 3000);
+	  
+
+		$scope.send = function(message) {
+		   if (socket.readyState === WebSocket.OPEN) {
+		      	socket.send(message);
+		    } else {
+		    	console.log('The socket is not open.');
+		    }
+		}		
+		
+		$scope.subscribe = function(subId) {
+	 	   if (socket.readyState === WebSocket.OPEN) {
+				var subMsg = new ArrayBuffer(2);			
+				var byteView = new Uint8Array(subMsg);
+				byteView[0] = subId;
+				byteView[1] = 1;//subscribe			
+			   	socket.send(subMsg);	
+		    } else {
+		    	console.log('The socket is not open.');
+		    }
+		}
+		
+		$scope.unsubscribe = function(subId) {
+		   if (socket.readyState === WebSocket.OPEN) {
+				var subMsg = new ArrayBuffer(2);			
+				var byteView = new Uint8Array(subMsg);
+				byteView[0] = subId;
+				byteView[1] = 0;//unsubscribe	
+			   	socket.send(subMsg);
+		    } else {
+		    	console.log('The socket is not open.');
+		    }
+		}  
+		
+		
+		function updatePumpStatus() {
     $scope.pumpStatus =
       'The pump simulation is ' +
       ($scope.pumpSimulationRunning ? 'running' : 'stopped') + '.';
@@ -76,14 +145,11 @@ app.controller('MyCtrl', $scope => {
 
   updatePumpStatus();
   updateTankStatus();
-
-  $scope.send = message => {
-    if (socket.readyState === WebSocket.OPEN) {
-      socket.send(message);
-    } else {
-      alert('The socket is not open.');
-      open(); //TODO: Where is this defined?
-    }
-  };
-});
-console.log('app.js x: created myCtrl');
+		
+		
+		
+		
+		
+		
+		    
+ }));
